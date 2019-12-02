88104cadabf2b3603a28466f90fc05b5a8618166
trvinh <trvinh@gmail.com>
2019-12-02 10:59:46 +0100

    first pkg for hamstr

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -4,19 +4,13 @@ set -e
 BINARY_HOME=$PREFIX/bin
 TESTPKG_HOME=$PREFIX/$PKG_NAME-$PKG_VERSION
 
-# Run setup
-./bin/setup.sh
-
 # Copy source to the conda environment
 mkdir -p $TESTPKG_HOME
-# cp -R $SRC_DIR/* $TESTPKG_HOME/
-cp -R * $TESTPKG_HOME/
+cp -R $SRC_DIR/* $TESTPKG_HOME/
 
 # Create symbolic links for a5_miseq's launch script
 mkdir -p $BINARY_HOME
-#ln -s $TESTPKG_HOME/bin/testPkg.pl $BINARY_HOME/
-ln -s $TESTPKG_HOME/bin/*.pl $BINARY_HOME/
-#chmod +x $BINARY_HOME/testPkg.pl
-chmod +x $BINARY_HOME/*.pl
+ln -s $TESTPKG_HOME/bin/setup_hamstr.sh $BINARY_HOME/setup_hamstr
+chmod +x $BINARY_HOME/setup_hamstr
 
 export PKGTEST="$BINARY_HOME"
